b66806cd0e3f66d39de2e8d164b1b9f9226dcb35
Igor Amorim <igoramorin6@gmail.com>
2023-10-19 22:06:29 -0300

    Igor Amorim

--- asset/js/script.js
+++ asset/js/script.js
@@ -9,3 +9,20 @@ document.addEventListener('scroll', () =>{
     let header = document.querySelector('.navBar')
     header.classList.toggle('rolagem', scrollY > 10)
 })
+
+const btnProduto = document.getElementById("btn-produto")
+const dropdownMenu = document.getElementById("dropdown-menu")
+
+btnProduto.addEventListener('click', ()=> {
+    dropdownMenu.classList.toggle('active')
+})
+
+
+// Mobile dropdown
+const btnProdutoMobile = document.getElementById("btn-produto-mobile")
+const dropdownMenuMobile = document.getElementById("dropdown-menu-mobile")
+
+btnProdutoMobile.addEventListener('click', ()=> {
+    dropdownMenuMobile.classList.toggle('mobile')
+})
+// Mobile dropdown
